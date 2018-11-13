a87cca943f0b0e35aea4bfec0bc7f19fb46f86ab
Mohamad al moualem <m.almouallem@gmail.com>
2018-11-13 10:51:45 +0100

    Fix: check, in

--- check/check.go
+++ check/check.go
@@ -1,7 +1,9 @@
-package out
+package main
 
 import (
+	"encoding/json"
 	"log"
+	"os"
 )
 
 type (
@@ -26,9 +28,21 @@ type (
 	outOutputJSON inOutputJSON
 )
 
+// createOutput ...
+func createOutput(output interface{}, encoder *json.Encoder, logger *log.Logger) error {
+	b, err := json.MarshalIndent(output, "", "  ")
+	if err != nil {
+		return err
+	}
+	logger.Print(string(b))
+
+	// encode output to stdout
+	return encoder.Encode(output)
+
+}
+
 // Check ...
 func Check(input InputJSON, logger *log.Logger) (outOutputJSON, error) {
-
 	var ref = input.Version.Ref
 
 	// OUTPUT
@@ -41,5 +55,29 @@ func Check(input InputJSON, logger *log.Logger) (outOutputJSON, error) {
 	}
 
 	return output, nil
+
+}
+
+func main() {
+
+	var (
+		input   InputJSON
+		decoder = json.NewDecoder(os.Stdin)
+		encoder = json.NewEncoder(os.Stdout)
+		logger  = log.New(os.Stderr, "resource:", log.Lshortfile)
+	)
+
+	if err := decoder.Decode(&input); err != nil {
+		logger.Fatalf("Failed to decode to stdin: %s", err)
+	}
+
+	output, err := Check(input, logger)
+	if err != nil {
+		logger.Fatalf("Input missing a field: %s", err)
+	}
+
+	if err := createOutput(output, encoder, logger); err != nil {
+		logger.Fatalf("Failed to encode to stdout: %s", err)
+	}
 
 }
--- in/in.go
+++ in/in.go
@@ -1,7 +1,9 @@
-package out
+package main
 
 import (
+	"encoding/json"
 	"log"
+	"os"
 )
 
 type (
@@ -26,6 +28,19 @@ type (
 	outOutputJSON inOutputJSON
 )
 
+// createOutput ...
+func createOutput(output interface{}, encoder *json.Encoder, logger *log.Logger) error {
+	b, err := json.MarshalIndent(output, "", "  ")
+	if err != nil {
+		return err
+	}
+	logger.Print(string(b))
+
+	// encode output to stdout
+	return encoder.Encode(output)
+
+}
+
 // In ...
 func In(input InputJSON, logger *log.Logger) (outOutputJSON, error) {
 	var ref = input.Version.Ref
@@ -40,5 +55,29 @@ func In(input InputJSON, logger *log.Logger) (outOutputJSON, error) {
 	}
 
 	return output, nil
+
+}
+
+func main() {
+
+	var (
+		input   InputJSON
+		decoder = json.NewDecoder(os.Stdin)
+		encoder = json.NewEncoder(os.Stdout)
+		logger  = log.New(os.Stderr, "resource:", log.Lshortfile)
+	)
+
+	if err := decoder.Decode(&input); err != nil {
+		logger.Fatalf("Failed to decode to stdin: %s", err)
+	}
+
+	output, err := In(input, logger)
+	if err != nil {
+		logger.Fatalf("Input missing a field: %s", err)
+	}
+
+	if err := createOutput(output, encoder, logger); err != nil {
+		logger.Fatalf("Failed to encode to stdout: %s", err)
+	}
 
 }
